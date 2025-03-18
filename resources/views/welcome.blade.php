2b04ad7adf5cb2dcd62d2e1a85d4607fa9b9f35e
VelosoN97 <nicolas.veloso0397@gmail.com>
2025-03-18 14:52:28 -0300

    Commit 3 - Integración de JQuery al chatbot

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,3 +56,4 @@
     </div>
 </body>
 </html>
+<script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
